f6c43db8cbc92d34d1f0004f2170ddd7fec51857
OLIVIER AUBERGER <oliviera89@gmail.com>
2019-10-17 15:46:13 -0500

    All files created. Started writing js

---
+++ bamazonCustomer.js
@@ -0,0 +1,7 @@
+
+var inquirer = require("inquirer");
+var mysql = require('mysql');
+
+//establish connection between js file and sql database
+
+//Ask questions to the user and treat them as requests
