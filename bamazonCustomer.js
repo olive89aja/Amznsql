8aa97c5686d0ca5d168b5652ff9f007d1758703b
OLIVIER AUBERGER <oliviera89@gmail.com>
2019-10-19 19:29:16 -0500

    Now updating inventory for item 1 iPhone, one at a time. Searching for a solution for a bigger order

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
 //This code is strongly inspired by the class about the song and album databases
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+//var connection = mysql.createConnection({multipleStatements: true});
+
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -36,20 +38,40 @@ function productSearch() {
             console.log("Here are your results" + JSON.stringify(res,null,10));
         
             if(res.length === 0) {console.log("Insufficient quantity!"); }
-           
-            //To remember what the user decided to input
-            // console.log(response); 
+   
+   
+        connection.query("UPDATE bamazon.products SET stock_quantity = stock_quantity -1 WHERE item_id=1", function(err,res) {
+         if(err) throw err;
+         console.log("Inventory updated"+JSON.stringify(res,null,10));
+         console.log("Have a nice day");         
+          
         
         })
 
       
     })
-    // connection.query("UPDATE stock_quantity WHERE item_id=?", [response.x2,response.x1], function(err,res) {
-    //     if(err) throw err;
-    //     if(res) console.log("Thank you for your purchase");
-    //     else console.log("Have a nice day"); 
-}//)}
+    
+})};
 
 productSearch();
 
 
+//  productPurchase()
+//  { inquirer.prompt([
+//         {
+//             type: "number",
+//             message: "What is the id, between 1 and 10, of the product you would like to buy ?",
+//             name: "x1"
+//         },
+//         {
+//             type: "number",
+//             message: "How many units of this product would you like to buy ?",
+//             name: "x2"
+//         }
+//     ]).then(response => {
+   
+     
+// })
+// })}
+
+// productPurchase();
